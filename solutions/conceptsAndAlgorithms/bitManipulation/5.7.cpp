86afd238f6a3f46620239373604c0712be42d898
shyal <shyal@shyal.com>
2015-09-11 14:11:33 +0200

    adding solution 5.7

--- solutions/conceptsAndAlgorithms/bitManipulation/5.7.cpp
+++ solutions/conceptsAndAlgorithms/bitManipulation/5.7.cpp
@@ -1,37 +1,32 @@
 #include "5.7.h"
 #include <bitset>
 #include <iostream>
+#include <assert.h>
+#include "lib/datastructures/vector.h"
 
 using namespace std;
 
-void test5_7()
+int findMissingElementInArray(int missingElement)
 {
-    int ar[19] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19};
+    int arraySize = missingElement + 10 - 1;
+    ctci::vector<int> ar(arraySize);
+    for (int i = 0, j = 0; i < arraySize; i++, j == missingElement-1 ? j+=2 : j++)
+            ar[i] = j;
+
     int missing = -1;
     int t = 0;
 
-    for (int i = 0; i <= 19; i++)
+    for (int i = 0; i <= arraySize; i++)
     {
-        int arval = ar[i];
-        short b0 = bitset<sizeof(i)*8>(arval)[0];
-        if ((t == 0 || t == 6))
+        short b0 = bitset<sizeof(i)*8>(ar[i])[0];
+        if ((t == 0 || t == 6) && b0 == 0)
         {
-            if (b0 == 0)
-            {
-            }
-            else
-            {
-                missing = ar[i]-1;
-                break;
-            }
         }
         else if (t % 2 == b0)
         {
-
         }
         else if (t % 2 != 0 && b0 == 1)
         {
-
         }
         else
         {
@@ -40,5 +35,10 @@ void test5_7()
         }
         t = t == 9 ? 0 : t + 1;
     }
-    cout << "missing is " << missing << endl;
+    return missing;
+}
+
+void test5_7()
+{
+    assert(findMissingElementInArray(14) == 14);
 }
