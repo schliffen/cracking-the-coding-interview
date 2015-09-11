e9b45bd3a2f438e6716e7454cc7a5a0280f60497
shyal <shyal@shyal.com>
2015-09-11 16:47:56 +0200

    added solution 5.7

--- solutions/conceptsAndAlgorithms/bitManipulation/5.7.cpp
+++ solutions/conceptsAndAlgorithms/bitManipulation/5.7.cpp
@@ -2,32 +2,32 @@
 #include <bitset>
 #include <iostream>
 #include <assert.h>
+#include <limits>
 #include "lib/datastructures/vector.h"
+
+// 5.7 An array A contains all the integers from 0 to n, except for one number which is missing.
+// In this problem, we cannot access an entire integer in A with a single operation.
+// The elements of A are represented in binary, and the only operation we can use to access
+// them is "fetch the jth bit of A[i]," which takes constant time. Write code to find the
+// missing integer. Can you do it in 0(n) time?
 
 using namespace std;
 
-int findMissingElementInArray(int missingElement)
+short fetchTheJthBitOfAi(ctci::vector<int>& A, unsigned int j, unsigned int i)
+{
+    return bitset<sizeof(i) * CHAR_BIT>(A[i])[j];
+}
+
+unsigned int findMissingElementInArray(unsigned int missingElement)
 {
     size_t arraySize = missingElement + 10 - 1;
     ctci::vector<int> ar(arraySize);
     for (size_t i = 0; i < arraySize; i++)
-            ar[i] = i;
-
+        ar[i] = i;
     ar.erase(ar.begin() + missingElement);
-
-    int t = 0;
-
-    for (size_t i = 0; i <= arraySize; i++)
-    {
-        short b0 = bitset<sizeof(i)*8>(ar[i])[0];
-
-        if ((t == 0 || t == 6) && b0 == 0){}
-        else if (t % 2 == b0){}
-        else if (t % 2 != !b0){}
-        else
-        {
+    for (size_t i = 0, t = 0; i <= arraySize; i++) {
+        if (t % 2 != fetchTheJthBitOfAi(ar, 0, i))
             return i;
-        }
         t = t == 9 ? 0 : t + 1;
     }
     return 0;
@@ -35,8 +35,7 @@ int findMissingElementInArray(int missingElement)
 
 void test5_7()
 {
-    assert(findMissingElementInArray(0) == 0);
-    assert(findMissingElementInArray(6) == 6);
-    assert(findMissingElementInArray(14) == 14);
-    assert(findMissingElementInArray(1e4+14) == 1e4+14);
+    for (size_t i = 0; i < 100; i++)
+        assert(findMissingElementInArray(i) == i);
+    cout << "5.7 passed!" << endl;
 }
